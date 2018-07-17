e1f506b9eeaf27b0e022709c54b3a41e027e73cf
Geert Huls <geert.huls82@gmail.com>
2018-07-17 22:13:42 +0200

    Call correct add endpoint from client

--- TourManagement.Client/src/app/tours/shared/tour.service.ts
+++ TourManagement.Client/src/app/tours/shared/tour.service.ts
@@ -7,11 +7,13 @@ import 'rxjs/add/operator/do';
 
 import { Tour } from './tour.model';
 import { BaseService } from '../../shared/base.service';
-import {TourWithEstimatedProfits} from "./tour-with-estimated-profits.model";
-import {TourForCreation} from "./tour-for-creation.model";
-import {TourWithManagerForCreation} from "./tour-with-manager-for-creation.model";
-import {TourWithShows} from "./tour-with-shows.model";
-import {TourWithEstimatedProfitsAndShows} from "./tour-with-estimated-profits-and-shows.model";
+import {TourWithEstimatedProfits} from './tour-with-estimated-profits.model';
+import {TourForCreation} from './tour-for-creation.model';
+import {TourWithManagerForCreation} from './tour-with-manager-for-creation.model';
+import {TourWithShows} from './tour-with-shows.model';
+import {TourWithEstimatedProfitsAndShows} from './tour-with-estimated-profits-and-shows.model';
+import {TourWithShowsForCreation} from './tour-with-shows-for-creation.model';
+import {TourWithManagerAndShowsForCreation} from './tour-with-manager-and-shows-for-creation.model';
 
 @Injectable()
 export class TourService extends BaseService {
@@ -51,5 +53,15 @@ export class TourService extends BaseService {
     addTourWithManager(tourToAdd: TourWithManagerForCreation): Observable<Tour> {
       return this.http.post<Tour>(`${this.apiUrl}/tours`, tourToAdd,
         { headers: { 'Content-Type': 'application/vnd.marvin.tourwithmanagerforcreation+json' } });
+    }
+
+    addTourWithShows(tourToAdd: TourWithShowsForCreation): Observable<Tour> {
+      return this.http.post<Tour>(`${this.apiUrl}/tours`, tourToAdd,
+        { headers: { 'Content-Type': 'application/vnd.marvin.tourwithshowsforcreation+json' } });
+    }
+
+    addTourWithManagerAndShows(tourToAdd: TourWithManagerAndShowsForCreation): Observable<Tour> {
+      return this.http.post<Tour>(`${this.apiUrl}/tours`, tourToAdd,
+        { headers: { 'Content-Type': 'application/vnd.marvin.tourwithmanagerandshowsforcreation+json' } });
     }
 }
