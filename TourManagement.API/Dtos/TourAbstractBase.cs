c428d23e0ccb9925a022fab4a1de0c81632a2ab4
Geert Huls <geert.huls82@gmail.com>
2018-07-18 22:22:12 +0200

    Server side data validation use data annotations

--- TourManagement.API/Dtos/TourAbstractBase.cs
+++ TourManagement.API/Dtos/TourAbstractBase.cs
@@ -1,15 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace TourManagement.API.Dtos
 {
     public abstract class TourAbstractBase
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required.")]
+        [MaxLength(200, ErrorMessage = "Title is too long.")]
         public string Title { get; set; }
-        public string Description { get; set; }
+
+        [MaxLength(2000, ErrorMessage = "Description is too long.")]
+        public virtual string Description { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "The start date is required.")]
         public DateTimeOffset StartDate { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "The end date is required.")]
         public DateTimeOffset EndDate { get; set; }
     }
 }
