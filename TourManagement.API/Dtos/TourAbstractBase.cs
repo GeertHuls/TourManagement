6ce194f1e52f28b4d482912b72eac2f987cdade2
Geert Huls <geert.huls82@gmail.com>
2018-07-18 22:34:38 +0200

    Client side form validation

--- TourManagement.API/Dtos/TourAbstractBase.cs
+++ TourManagement.API/Dtos/TourAbstractBase.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace TourManagement.API.Dtos
 {
-    public abstract class TourAbstractBase
+    public abstract class TourAbstractBase : IValidatableObject
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Title is required.")]
         [MaxLength(200, ErrorMessage = "Title is too long.")]
@@ -17,5 +18,16 @@ namespace TourManagement.API.Dtos
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "The end date is required.")]
         public DateTimeOffset EndDate { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!(StartDate < EndDate))
+            {
+                yield return new ValidationResult(
+                    "The start date should be smaller than the end date.",
+                    new[] { "Tour" });
+            }
+
+        }
     }
 }
